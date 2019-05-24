b7147c2ec08e1a77bf309be21094a68c0add674e
Mohan SPM <40892986+cloudstarmohan@users.noreply.github.com>
2019-05-24 16:08:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2019/05/25';
+        var finalDate = '2019/05/31';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
